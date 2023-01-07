6b68caf50d4704b9bdc53b7e351e5be99bb483f9
Lena k <ktyecz09@mail.ru>
2023-01-07 20:11:57 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int balanse = 100;// начальный баланс
+        int amount = 1200; // пополнение
+        int condition = 1000; //условия пополнения
+        int bonus = amount/100 ;
+
+        if (amount > condition) {
+            bonus = amount / 100;
+        }
+        {
+            System.out.println(amount + balanse + bonus);
+            System.out.println(bonus);
+        }
+
+    }}
