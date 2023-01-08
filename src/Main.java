7ec04ac6cf8265ef64cece6def214e810108807e
Lena k <ktyecz09@mail.ru>
2023-01-08 18:35:41 +0300

    add Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         int balanse = 100;// начальный баланс
-        int amount = 1200; // пополнение
+        int amount = 1500; // пополнение
         int condition = 1000; //условия пополнения
-        int bonus = amount / 100;
-
+        int bonus;
         if (amount > condition) {
             bonus = amount / 100;
+        } else {
+            bonus = 0;
         }
-        {
-            System.out.println(amount + balanse + bonus);
-            System.out.println(bonus);
-        }
+        System.out.println(amount + balanse + bonus);
+        System.out.println(bonus);
+
 
     }
 }
